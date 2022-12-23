8ceb1e3f908333d52c6e72aa5270d9eef51f1bb5
Justin Turner <48540310+ProjectGrantwood@users.noreply.github.com>
2022-12-23 14:05:08 -0500

    Adjusted colors on Project 5

--- Project 5/sea_level_predictor.py
+++ Project 5/sea_level_predictor.py
@@ -12,25 +12,25 @@ def draw_plot():
     # *** In addition, I'm setting x and y limits on the plot axes to pass all automated tests
     ax.set_xlim(left=1850, right=2075)
     ax.set_ylim(bottom=-1, top=17)
-    ax.scatter(df['Year'], df['CSIRO Adjusted Sea Level'], 4, c='b')
+    ax.scatter(df['Year'], df['CSIRO Adjusted Sea Level'], 4, c='w')
 
     # Create first line of best fit
     linreg_1 = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
-    # *** Using ax.add_line to meet the project requirements that the regression line data
+    # *** we're using ax.add_line() to meet the project requirements that the regression line data
     # *** conform to a specific size with exact values.
     # *** In practice, I think ax.axes.axline looks better, and is also quicker to code
-    # *** because it can take a point tuple and a slope as a parameter, eliminating the need for the lower-level
-    # *** math you see in the lines below.
+    # *** because it can take a point tuple and a slope as a parameter, eliminating the need
+    # *** for the lower-level math you see in the lines below.
     l1_xdata = np.linspace(1880, 2050, 171)
     l1_ydata = np.linspace(1880 * linreg_1.slope + linreg_1.intercept, linreg_1.slope * 2050 + linreg_1.intercept, 171)
-    ax.add_line(plt.Line2D(l1_xdata, l1_ydata, color='w', linestyle=':'))
+    ax.add_line(plt.Line2D(l1_xdata, l1_ydata, linewidth=2.5, color='g', linestyle='-'))
 
     # Create second line of best fit
     df_recent = df[df['Year'] >= 2000]
     linreg_2 = linregress(df_recent['Year'], df_recent['CSIRO Adjusted Sea Level'])
     l2_xdata = np.linspace(2000, 2050, 51)
     l2_ydata = np.linspace(2000 *linreg_2.slope + linreg_2.intercept, linreg_2.slope * 2050 + linreg_2.intercept, 51)
-    ax.add_line(plt.Line2D(l2_xdata, l2_ydata, color="y", linestyle=":"))
+    ax.add_line(plt.Line2D(l2_xdata, l2_ydata, linewidth=2.5, color="r", linestyle="-"))
 
     # Add labels and title
     ax.set_title('Rise in Sea Level')
@@ -42,4 +42,4 @@ def draw_plot():
 
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
-    return plt.gca(), ax
+    return plt.gca()
